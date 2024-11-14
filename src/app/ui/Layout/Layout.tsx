ec6867ba809beee38c2a2ba33e289518f51c0e70
alex <alexevansdance@gmail.com>
2024-11-14 22:28:32 +0300

    change logo, add test icon

--- src/app/ui/Layout/Layout.tsx
+++ src/app/ui/Layout/Layout.tsx
@@ -1,13 +1,17 @@
-import { Layout, Menu, theme } from 'antd'
+import { RadarChartOutlined } from '@ant-design/icons'
+import { Button, Layout, Menu, theme } from 'antd'
 import { Content, Header } from 'antd/es/layout/layout'
 import Sider from 'antd/es/layout/Sider'
-import type { FC } from 'react'
+import { useState, type FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Logo } from '../../../assets'
 
 const { useToken } = theme
 export const AppLayout: FC = () => {
   const { token } = useToken()
+
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
     <Layout>
       <Header
@@ -30,15 +34,36 @@ export const AppLayout: FC = () => {
             minHeight: `calc(100dvh - ${token.Layout?.headerHeight}px)`,
           }}
         >
-          <Sider width={350} style={{ position: 'fixed', padding: '24px' }}>
+          <Sider
+            collapsed={collapsed}
+            width={350}
+            collapsedWidth={150}
+            style={{
+              position: 'fixed',
+              padding: '24px',
+              minHeight: `calc(100dvh - ${token.Layout?.headerHeight}px)`,
+            }}
+          >
             <Menu
               mode="inline"
               style={{
                 borderRadius: '16px',
-                height: '800px',
+                height: '500px',
               }}
-              items={[{ key: '1', label: '1' }]}
+              items={[{ key: '1', label: '1', icon: <RadarChartOutlined /> }]}
             />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Button
+                style={{
+                  position: 'absolute',
+                  width: !collapsed ? 150 : 50,
+                  bottom: 50,
+                }}
+                onClick={() => setCollapsed((prev) => !prev)}
+              >
+                Collapse
+              </Button>
+            </div>
           </Sider>
           <div style={{ width: 350 }}></div>
           <Content style={{ padding: '24px', minHeight: 280 }}>
